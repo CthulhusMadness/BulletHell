c77702802cff9d5977ccf19d460cc732913b842a
Michele Santoni <mikysanto.97@gmail.com>
2020-03-20 00:38:41 +0100

    [TECH] add new TestScene; add player in the scene; add inputData SO; now the player moves and turn towards where the mouse pointer is;

---
+++ BulletHell/Assets/Scripts/InputData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InputData", menuName = "Input")]
+public class InputData : ScriptableObject
+{
+    #region Fields
+
+    public KeyCode Up = KeyCode.W;
+    public KeyCode Down = KeyCode.S;
+    public KeyCode Left = KeyCode.A;
+    public KeyCode Right = KeyCode.D;
+    public KeyCode FirstWeapon = KeyCode.Mouse0;
+    public KeyCode SecondaryWeapon = KeyCode.Mouse1;
+
+    #endregion
+
+    #region UnityCallbacks
+
+
+
+    #endregion
+
+    #region Methods
+
+
+
+    #endregion
+}
--- BulletHell/Assets/Scripts/PlayerInput.cs
+++ BulletHell/Assets/Scripts/PlayerInput.cs
@@ -1,18 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Fields
+
+    [SerializeField] private InputData inputData;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform graphics = null;
+    [SerializeField] private float speed = 5f;
+    
+
+    #endregion
+
+    #region UnityCallbacks
+
+    private void Start()
     {
-        
+        Debug.Log(inputData);
+        if (inputData == null)
+        {
+            inputData = ScriptableObject.CreateInstance<InputData>();
+        }
+
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        GetInput();
     }
+
+
+    #endregion
+
+    #region Methods
+
+    private void GetInput()
+    {
+        // Keyboard
+        float horizontal = Convert.ToInt32(Input.GetKey(inputData.Right)) - Convert.ToInt32(Input.GetKey(inputData.Left));
+        float vertical = Convert.ToInt32(Input.GetKey(inputData.Up)) - Convert.ToInt32(Input.GetKey(inputData.Down));
+        float step = speed * Time.deltaTime;
+        Vector3 movement = Vector3.zero;
+        if (horizontal != 0 || vertical != 0)
+        {
+            movement = new Vector3(horizontal, 0, vertical).normalized;
+        }
+        
+        transform.Translate(movement * step);
+        
+        // mouse
+        Vector2 viewportPosition = (Vector2)cam.ScreenToViewportPoint(Input.mousePosition) - new Vector2(.5f, .5f);
+        Vector3 mousePosition = new Vector3(viewportPosition.x, 0, viewportPosition.y);
+        graphics.LookAt(transform.position + mousePosition);
+    }
+    
+    #endregion
 }
